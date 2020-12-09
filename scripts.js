5610eb1967145e26df5299764316d2dede6447ca
Alex <alexsnow73@gmail.com>
2020-12-09 15:26:23 +0200

    Test

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
+"use strict";
 let a = "Alex", g = "Gala";
 console.log(a, g);
+
+const answer = prompt("Вам есть 18 лет?", "");
+console.log(answer);
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 console.log(numberOfFilms);
