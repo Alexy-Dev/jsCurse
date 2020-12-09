12454390e0924bff13d8dd223b148b00041a9172
Alex <alexsnow73@gmail.com>
2020-12-09 15:09:39 +0200

    krakenWithUs

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ const user = "Alex";
 alert(`Hello, ${user}`);
 
 console.log('arr' + " - object");
-console.log(4 + +"5")
+console.log(4 + +"5");
 
 let incr = 10, decr = 10;
 
--- scripts.js
+++ scripts.js
@@ -1,2 +1,6 @@
 let a = "Alex", g = "Gala";
 console.log(a, g);
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+console.log(numberOfFilms);
+
