104ad7637605c636fccdeac04b58bb413c158cc6
Alex <alexsnow73@gmail.com>
2020-12-22 11:01:03 +0200

    practice 2

---
+++ test.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// for (let i = 1; i < 47; i++) {
+//   if (i != 5 && i != 15 && i != 25) {
+//     continue;
+// } 
+//   console.log(i + 1);  
+// }
+
+const userAlex = {
+  name: 'Alex',
+  userName: 'alexUser',
+  age: 40,
+};
+
+for (let i = 1; i < 5; i++) {
+  console.log(userAlex);
+}
+// while (userAlex.age <= 47) {
+//   console.log(userAlex.name);
+//   userAlex.age++;  
+// }
+//   if (userAlex.name == 'Alex' && userAlex.age == 47) {
+//    console.log(userAlex);
+// } else if (userAlex.age == 47) {
+//   console.log(userAlex.age);  
+// } else {
+//   console.log(userAlex.name + ' ' + userAlex.userName + ' ' + userAlex.age);
+// }
+
+// switch (userAlex) {
+//       case userAlex.name == 'Alex':
+//            console.log('yes');
+//           break;
+//           case userAlex.age == 47:
+//               console.log('Yes');
+//               break;
+//           default:
+//               console.log('enybodyhome?');
+//               break;   
+//   }
